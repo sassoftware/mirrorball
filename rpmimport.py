99a3f849735b986dad774a0926f467398c2b4b4b
Xiaowen Xin <https://issues.rpath.com/>
2006-11-16 17:31:13 +0100

    Broke info-* creation into its own class.
    committer: Xiaowen Xin <https://issues.rpath.com/>

--- rpmimport.py
+++ rpmimport.py
@@ -201,144 +201,6 @@ class RecipeMaker:
         self.repos = repos
         self.rpmSource = rpmSource
 
-    def closeUser(self, user, users, groups, ustaging, gstaging, ugMap):
-        """
-        If a user has supplemental groups, then create those.
-        """
-        try:
-            uprop = pwd.getpwnam(user)
-        except KeyError, e:
-            print "The user %s does not exist on the build system." \
-                "  Please create it before running this." % (user)
-            return
-
-        (uid, gid, comment, homedir, shell) = uprop[2:]
-        gprop = grp.getgrgid(gid)
-        group = gprop[0]
-        supgroups = ugMap.get(user, set()).difference(set([group]))
-        for g in supgroups:
-            if g not in groups:
-                gstaging.add(g)
-        users.add(user)
-
-    def closeGroup(self, group, users, groups, ustaging, gstaging, ugMap):
-        """
-        If there is a user with this as its primary group, then create the user
-        instead.
-        """
-
-        try:
-            gprop = grp.getgrnam(group)
-        except KeyError, e:
-            print "The group %s does not exist on the build system." \
-                "  Please create it first before running this." % (group)
-            return
-
-        gid = gprop[2]
-        primaries = gprop[3] + [group]
-        for u in primaries:
-            try:
-                uprop = pwd.getpwnam(u)
-                if uprop[3] == gid:
-                    ustaging.add(uprop[0])
-                    return
-            except KeyError, e:
-                # No such user, oh well, ignore.
-                pass
-        groups.add(group)
-
-    def walkUsers(self):
-        # Map username to groups it belongs to.
-        ugMap = dict()
-        for (g, a, b, us) in grp.getgrall():
-            for u in us:
-                if u in ugMap:
-                    ugMap[u].add(g)
-                else:
-                    ugMap[u] = set([g])
-
-        # Get all users and groups used in this run.
-        users = set()
-        groups = set()
-        for src in self.rpmSource.getSrpms():
-            for rpm in self.rpmSource.rpmMap[src].values():
-                header = self.rpmSource.getHeader(rpm)
-                users = users.union(header[FILEUSERNAME])
-                groups = groups.union(header[FILEGROUPNAME])
-
-        # Groups and users depend on each other, so do their closure.
-        ustaging = users
-        gstaging = groups
-        users = set()
-        groups = set()
-        while ustaging or gstaging:
-            for user in ustaging:
-                self.closeUser(user, users, groups, ustaging, gstaging, ugMap)
-            ustaging = set()
-            for group in gstaging:
-                self.closeGroup(group, users, groups, ustaging, gstaging, ugMap)
-            gstaging = set()
-
-        # Remove the root user and group.
-        users = users.difference(['root'])
-        groups = groups.difference(['root'])
-
-        # If there are groups named the same as the user, then the user must
-        # have this group as its primary group.
-        groups = groups.difference(users)
-
-        # All the packages we might create.
-        srccomps = {}
-        for account in users.union(groups):
-            srccomps['info-%s:source' % (account)] = {cfg.buildLabel: None}
-
-        # Get current repository contents.
-        repoContents = self.repos.getTroveVersionsByLabel(srccomps)
-
-        # Create users.
-        for user in users:
-            # If it already exists, then move on.
-            if 'info-%s:source' %user in repoContents:
-                continue
-
-            uprop = pwd.getpwnam(user)
-            (uid, gid, comment, homedir, shell) = uprop[2:]
-            gprop = grp.getgrgid(gid)
-            group = gprop[0]
-            supgroups = ugMap.get(user, set()).difference(set([group]))
-
-            self.create('info-%s' % (user),
-                "class info_%(user)s(UserInfoRecipe):\n"
-                "    name = 'info-%(user)s'\n"
-                "    version = '1'\n"
-                "\n"
-                "    def setup(r):\n"
-                "        r.User('%(user)s', %(uid)s, group='%(group)s', groupid=%(gid)s,\n"
-                "            homedir='%(homedir)s', comment='%(comment)s', shell='%(shell)s',\n"
-                "            supplemental=[%(supgroups)s])\n"
-                % dict(user=user, uid=uid, group=group, gid=gid,
-                    homedir=homedir, comment=comment, shell=shell,
-                    supgroups=', '.join("'%s'" % g for g in supgroups)))
-
-        # Create groups.
-        for group in groups:
-            # If it already exists, then move on.
-            if 'info-%s:source' %group in repoContents:
-                continue
-
-            gprop = grp.getgrnam(group)
-            gid = gprop[2]
-
-            # Create the group recipe.
-            self.create('info-%s' % (group),
-                "class info_%(group)s(GroupInfoRecipe):\n"
-                "    name = 'info-%(group)s'\n"
-                "    version = '1'\n"
-                "\n"
-                "    def setup(r):\n"
-                "        r.Group('%(group)s', %(gid)s)\n"
-                % dict(group=group, gid=gid))
-
     def create(self, pkgname, recipeContents, srpm = None):
         print 'creating initial template for', pkgname
         try:
@@ -413,4 +275,16 @@ if __name__ == '__main__':
             pkgname = srccomp.split(':')[0]
             recipeMaker.create(pkgname, rpmSource.createTemplate(src), src)
 
-    recipeMaker.walkUsers()
+
+    # Get all users and groups used in this run.
+    users = set()
+    groups = set()
+    for src in rpmSource.getSrpms():
+        for rpm in rpmSource.rpmMap[src].values():
+            header = rpmSource.getHeader(rpm)
+            users = users.union(header[FILEUSERNAME])
+            groups = groups.union(header[FILEGROUPNAME])
+
+    import infoimport
+    infoMaker = infoimport.InfoMaker(repos, recipeMaker)
+    infoMaker.makeInfo(users, groups)
