05d140a2a2c3ad85bf64f28510a01ea67f018713
Elliot Peele <http://issues.rpath.com/>
2008-06-19 21:36:58 -0400

    replace recipemaker with internal methods fixup sanity checking add update and publish functionallity
    committer: Elliot Peele <http://issues.rpath.com/>

--- updatebot/update.py
+++ updatebot/update.py
@@ -19,11 +19,10 @@ Module for finding packages to update and updating them.
 import logging
 
 from rpmvercmp import rpmvercmp
-from rpmimport import recipemaker
 
 from updatebot import util
-from updatebot.errors import *
 from updatebot import conaryhelper
+from updatebot.errors import UpdateGoesBackwardsError, UpdateRemovesPackageError
 
 log = logging.getLogger('updatebot.update')
 
@@ -37,9 +36,6 @@ class Updater(object):
         self._rpmSource = rpmSource
 
         self._conaryhelper = conaryhelper.ConaryHelper(self._cfg)
-
-        ccfg = self._conaryhelper.getConaryConfig()
-        self._recipeMaker = recipemaker.RecipeMaker(ccfg, self._rpmSource)
 
     def getUpdates(self):
         """
@@ -92,8 +88,6 @@ class Updater(object):
                 # Add anything that has changed, version may have gone
                 # backwards if epoch changes.
                 troves.append(((name, version, flavor), latestSrpm))
-#            else:
-#                log.debug('found already up to date trove: %s' % ((name, version, flavor), ))
 
         log.info('found %s protentially updatable troves' % len(troves))
         return troves
@@ -130,8 +124,8 @@ class Updater(object):
         """
 
         needsUpdate = False
-        newNames = [ x.name for x in self._rpmSource.srcPkgMap[srpm] ]
-        manifest = [ x.strip() for x in self._recipeMaker.getManifest(nvf[0]) ]
+        newNames = [ (x.name, x.arch) for x in self._rpmSource.srcPkgMap[srpm] ]
+        manifest = self._conaryhelper.getManifest(nvf[0])
         for line in manifest:
             binPkg = self._rpmSource.locationMap[line]
             srcPkg = self._rpmSource.binPkgMap[binPkg]
@@ -145,20 +139,89 @@ class Updater(object):
                 raise UpdateGoesBackwardsError(why=(srcPkg, srpm))
 
             # make sure we aren't trying to remove a package
-            if binPkg.name not in newNames:
+            if (binPkg.name, binPkg.arch) not in newNames:
                 # Novell releases updates to only the binary rpms of a package
                 # that have chnaged. We have to use binaries from the old srpm.
                 # Get the last version of the pkg and add it to the srcPkgMap.
-                log.warn('using old version of package %s' % binPkg)
                 pkgs = self._rpmSource.binNameMap[binPkg.name]
-                pkgs.sort(util.packagevercmp)
+
+                # get the correct arch
+                pkg = None
+                for pkg in self._getLatestOfAvailableArches(pkgs):
+                    if pkg.arch == binPkg.arch:
+                        break
 
                 # Raise an exception if the versions of the packages aren't equal.
-                if rpmvercmp(pkgs[-1].version, binPkg.version) != 0:
+                if (rpmvercmp(pkg.epoch, srpm.epoch) != 0 or
+                    rpmvercmp(pkg.version, srpm.version) != 0):
                     raise UpdateRemovesPackageError(why='all rpms in the '
                             'manifest should have the same version, trying '
-                            'to add %s' % (pkgs[-1], ))
+                            'to add %s' % (pkg, ))
 
-                self._rpmSource.srcPkgMap[srpm].append(pkgs[-1])
+                log.warn('using old version of package %s' % (pkg, ))
+                self._rpmSource.srcPkgMap[srpm].append(pkg)
 
         return needsUpdate
+
+    def _getLatestOfAvailableArches(self, pkgLst):
+        """
+        Given a list of package objects, find the latest versions of each
+        package for each name/arch.
+        @param pkgLst: list of packages
+        @type pkgLst: [repomd.packagexml._Package, ...]
+        """
+
+        pkgLst.sort()
+
+        pkgMap = {}
+        for pkg in pkgLst:
+            key = pkg.name + pkg.arch
+            if key not in pkgMap:
+                pkgMap[key] = pkg
+                continue
+
+            # check if newer, last wins
+            if util.packagevercmp(pkg, pkgMap[key]) in (0, 1):
+                pkgMap[key] = pkg
+
+        ret = pkgMap.values()
+        ret.sort()
+
+        return ret
+
+    def update(self, nvf, srcPkg):
+        """
+        Update rpm manifest in source trove.
+        @param nvf: name, version, flavor tuple of source trove
+        @type nvf: tuple(name, versionObj, flavorObj)
+        @param srcPkg: package object for source rpm
+        @type srcPkg: repomd.packagexml._Package
+        """
+
+        manifest = self._getManifestFromRpmSource(srcPkg)
+        self._conaryhelper.setManifest(nvf[0], manifest,
+            commitMessage=self._cfg.commitMessage)
+
+    def _getManifestFromRpmSource(self, srcPkg):
+        """
+        Get the contents of the a manifest file from the rpmSource object.
+        @param srcPkg: source rpm package object
+        @type srcPkg: repomd.packagexml._Package
+        """
+
+        manifestPkgs = self._rpmSource.srcPkgMap[srcPkg]
+        pkgs = self._getLatestOfAvailableArches(manifestPkgs)
+        return [ x.location for x in pkgs ]
+
+    def publish(self, trvLst, expected, targetLabel):
+        """
+        Publish a group and its contents to a target label.
+        @param trvLst: list of troves to publish
+        @type trvLst: [(name, version, flavor), ... ]
+        @param expected: list of troves that are expected to be published.
+        @type expected: [(name, version, flavor), ...]
+        @param targetLabel: table to publish to
+        @type targetLabel: conary Label object
+        """
+
+        return self._conaryhelper.promote(trvLst, expected, targetLabel)
