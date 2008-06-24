e226965a7515ad3b87a50e8ca2b1d0035e302c39
Elliot Peele <http://issues.rpath.com/>
2008-06-24 16:10:37 -0400

    fix broken build tests add test for builder.build increase coverage to 100%
    committer: Elliot Peele <http://issues.rpath.com/>

--- test/unit_test/updatebottest/buildtest.py
+++ test/unit_test/updatebottest/buildtest.py
@@ -24,6 +24,66 @@ from updatebot import build
 from updatebot import errors
 
 class BuilderTest(slehelp.Helper):
+    def testBuild(self):
+        jobId = 1
+
+        mockVersion1 = mock.MockObject()
+        mockVersion2 = mock.MockObject()
+        mockFlavor1 = mock.MockObject()
+        mockFlavor2 = mock.MockObject()
+
+        trvSpecs = [('foo', mockVersion1, mockFlavor1),
+                    ('group-foo', mockVersion2, mockFlavor2),
+                   ]
+
+        contexts = ['a', 'b']
+
+        self.mock(self.updateBotCfg, 'archContexts', contexts)
+
+        troves = [('foo', mockVersion1, mockFlavor1, 'a'),
+                  ('foo', mockVersion1, mockFlavor1, 'b'),
+                  ('group-foo', mockVersion2, mockFlavor2),
+                 ]
+
+        trvMap = {('foo', mockVersion1, mockFlavor1, 'a'):
+                    [('foo', mockVersion1, mockFlavor1), ],
+                  ('foo', mockVersion1, mockFlavor1, 'b'):
+                    [('foo:lib', mockVersion1, mockFlavor1),
+                     ('foo', mockVersion1, mockFlavor1), ],
+                  ('group-foo', mockVersion2, mockFlavor2, None):
+                    [('group-foo-devel', mockVersion2, mockFlavor2), ],
+                 }
+
+        ret = {('foo', mockVersion1, None):
+                set([('foo', mockVersion1, mockFlavor1),
+                     ('foo:lib', mockVersion1, mockFlavor1),
+                    ]),
+               ('group-foo', mockVersion2, None):
+                set([('group-foo-devel', mockVersion2, mockFlavor2),
+                    ]),
+              }
+
+        mockStartJob = mock.MockObject()
+        mockMonitorJob = mock.MockObject()
+        mockSanityCheckJob = mock.MockObject()
+        mockCommitJob = mock.MockObject()
+
+        mockStartJob._mock.setReturn(jobId, troves)
+        mockCommitJob._mock.setReturn(trvMap, jobId)
+
+        builder = build.Builder(self.updateBotCfg)
+        self.mock(builder, '_startJob', mockStartJob)
+        self.mock(builder, '_monitorJob', mockMonitorJob)
+        self.mock(builder, '_sanityCheckJob', mockSanityCheckJob)
+        self.mock(builder, '_commitJob', mockCommitJob)
+
+        result = builder.build(trvSpecs)
+        self.failUnlessEqual(result, ret)
+        mockStartJob._mock.assertCalled(troves)
+        mockMonitorJob._mock.assertCalled(jobId)
+        mockSanityCheckJob._mock.assertCalled(jobId)
+        mockCommitJob._mock.assertCalled(jobId)
+
     def testStartJob(self):
         trvSpecs = (('foo', '', ''), )
 
@@ -42,9 +102,10 @@ class BuilderTest(slehelp.Helper):
 
     def testMonitorJob(self):
         mockMonitor = mock.MockObject()
-        self.mock(monitor, 'monitorJob', mockMonitor)
 
         builder = build.Builder(self.updateBotCfg)
+        self.mock(build.monitor, 'monitorJob', mockMonitor)
+
         builder._monitorJob(1)
 
         mockMonitor._mock.assertCalled(builder._helper.client, 1,
@@ -109,23 +170,26 @@ class BuilderTest(slehelp.Helper):
         mockHelper = mock.MockObject(stableReturnValues=True)
         mockHelper.getJob._mock.setReturn(mockJob, jobId)
         mockCommitJobs = mock.MockObject(stableReturnValues=True)
+        mockBuildCfg = mock.MockObject(stableReturnValues=True)
+        mockBuildCfg._mock.set(reposName='foo.example.com')
 
         self.mock(commit, 'commitJobs', mockCommitJobs)
 
         builder = build.Builder(self.updateBotCfg)
         builder._helper = mockHelper
+        builder._rmakeCfg = mockBuildCfg
 
         mockCommitJobs._mock.setDefaultReturn((True, sampleData))
         trvMap = builder._commitJob(jobId)
         self.failUnlessEqual(trvMap, expected)
         mockHelper.getJob._mock.assertCalled(jobId)
-        mockHelper.client.startCommit._mock.assertCalled(jobId)
+        mockHelper.client.startCommit._mock.assertCalled([jobId, ])
         mockHelper.client.commitSucceeded._mock.assertCalled(sampleData)
 
         mockCommitJobs._mock.setDefaultReturn((False, sampleData))
         self.failUnlessRaises(errors.CommitFailedError, builder._commitJob, jobId)
         mockHelper.getJob._mock.assertCalled(jobId)
-        mockHelper.client.startCommit._mock.assertCalled(jobId)
+        mockHelper.client.startCommit._mock.assertCalled([jobId, ])
         mockHelper.client.commitFailed._mock.assertCalled([jobId, ], sampleData)
         mockHelper.client.commitSucceeded._mock.assertNotCalled()
 
