0a07d5889d8742bc3f3bf9388bb9e22c09c30e91
Elliot Peele <http://bugs.rpath.com/>
2008-08-11 18:18:39 -0400

    don't print progress
    committer: Elliot Peele <http://bugs.rpath.com/>

--- scripts/sync-opensuse.sh
+++ scripts/sync-opensuse.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-rsync -arv --progress --exclude iso --exclude 10.* --exclude ppc --exclude ppc64 rsync://rsync.opensuse.org/opensuse-full /mnt/rpath/linux/
+rsync -arv --exclude iso --exclude 10.* --exclude ppc --exclude ppc64 rsync://rsync.opensuse.org/opensuse-full/opensuse/ /mnt/rpath/linux/opensuse
 
 ./hardlink.py /mnt/rpath/linux/opensuse
