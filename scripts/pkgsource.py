5c985cb420a804093127de190382d7f27a6a9bfd
Elliot Peele <http://bugs.rpath.com/>
2008-08-28 10:30:22 -0400

    load from $HOME
    committer: Elliot Peele <http://bugs.rpath.com/>

--- scripts/pkgsource.py
+++ scripts/pkgsource.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
+import os
 import sys
 from conary.lib import util
 sys.excepthook = util.genExcepthook()
+
+sys.path.insert(0, os.environ['HOME'] + '/hg/rpath-xmllib')
+sys.path.insert(0, os.environ['HOME'] + '/hg/conary')
+sys.path.insert(0, os.environ['HOME'] + '/hg/mirrorball')
 
 import logging
 import updatebot.log
@@ -15,7 +20,7 @@ from updatebot import config
 from updatebot import pkgsource
 
 cfg = config.UpdateBotConfig()
-cfg.read('/data/hg/mirrorball/config/ubuntu/updatebotrc')
+cfg.read(os.environ['HOME'] + '/hg/mirrorball/config/ubuntu/updatebotrc')
 
 client = Client('http://i.rdu.rpath.com/ubuntu')
 pkgSource = pkgsource.PackageSource(cfg)
