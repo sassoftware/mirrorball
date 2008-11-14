05eeb7a1d31959e3f163a54df9bec2136e569be3
Elliot Peele <https://issues.rpath.com/>
2008-11-14 17:40:51 -0500

    add new advisory model
    committer: Elliot Peele <https://issues.rpath.com/>

---
+++ updatebot/advisories/__init__.py
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2008 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+
+import logging
+from imputil import imp
+
+log = logging.getLogger('updatebot.advisories')
+
+class InvalidBackendError(Exception):
+    pass
+
+def __getBackend(backend):
+    if backend not in __supported_backends:
+        raise InvalidBackendError('%s is not a supported backend, please '
+            'choose from %s' % (backend, ','.join(__supported_backends)))
+
+    try:
+        path = [imp.find_module('updatebot.advisories')[1], ]
+        mod = imp.find_module(backend, path)
+        loaded = imp.load_module(backend, mod[0], mod[1], mod[2])
+        return loaded
+    except ImportError, e:
+        raise InvalidBackendError('Could not load %s backend: %s'
+                                  % (backend, e))
+
+def Advisor(cfg, pkgSource, backend='centos'):
+    klass = __getBackend(backend)
+    obj = klass(cfg, pkgSource)
+    return obj.load()
---
+++ updatebot/advisories/sles.py
@@ -0,0 +1,93 @@
+#
+# Copyright (c) 2008 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+
+from updatebot.advisories.common import BaseAdvisor, log
+
+class Advisor(BaseAdvisor):
+    def load(self):
+        """
+        Parse the required data to generate a mapping of binary package
+        object to patch object for a given platform into self._pkgMap.
+        """
+
+        for path, client in self._pkgSource.getClients().iteritems():
+            log.info('loading patch information %s' % path)
+            for patch in client.getPatchDetail():
+                self._lineOne(patch, path)
+
+    def _loadOne(self, patch, path):
+        """
+        Load one patch into mappings.
+        @param patch: repomd patch object
+        @type patch: repomd.patchxml._Patch
+        @param path: base path to repository
+        @type path: string
+        """
+
+        if self._filterPatch(patch):
+            return
+
+        for package in patch.packages:
+            package.location = path + '/' + package.location
+            if package not in self.pkgMap:
+                self.pkgMap[package] = set()
+            self.pkgMap[package].add(patch)
+
+    def _filterPatch(self, patch):
+        """
+        Filter out patches that match filters in config.
+        @param patch: repomd patch object
+        @type patch: repomd.patchxml._Patch
+        """
+
+        for _, regex in self._cfg.patchFilter:
+            if regex.match(patch.summary):
+                return True
+            if regex.match(patch.description):
+                return True
+
+        return False
+
+    def _hasException(self, binPkg):
+        """
+        Check the config for repositories with exceptions for sending
+        advisories. (io. repositories that we generated metadata for.)
+        @param binPkg: binary package object
+        @type binPkg: repomd.packagexml._Package
+        """
+
+        shortPath = binPkg.location.split('/')[0]
+
+        for advisoryException in self._cfg.advisoryException:
+            path = advisoryException[0].split('/')[0]
+            if path == shortPath:
+                return True
+        return False
+
+    def _isUpdatesRepo(self, binPkg):
+        """
+        Check the repository name. If this package didn't come from a updates
+        repository it is probably not security related.
+        @param binPkg: binary package object
+        @type binPkg: repomd.packagexml._Package
+        """
+
+        # msw agrees that this seems to be a sane check.
+
+        shortPath = binPkg.location.split('/')[0]
+
+        if shortPath.endswith('Updates'):
+            return True
+        else:
+            return False
