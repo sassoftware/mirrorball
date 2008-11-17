317902363b5df7ce2b9fe7fc71f5be8ba02844a8
Elliot Peele <https://issues.rpath.com/>
2008-11-17 18:35:48 -0500

    strip newlines from subjects
    committer: Elliot Peele <https://issues.rpath.com/>

--- pmap/common.py
+++ pmap/common.py
@@ -61,8 +61,7 @@ class BaseParser(Parser):
         self._curObj.fromAddr = fromLine[:fromLine.find('(')].replace(' at ', '@')
         self._curObj.fromName = fromLine[fromLine.find('('):].strip('()')
         self._curObj.timestamp = ' '.join(msg.get_from().split()[4:])
-        self._curObj.subject = msg['Subject']
-        self._curObj.subject.replace('\n\t', ' ')
+        self._curObj.subject = msg['Subject'].replace('\n\t', ' ')
 
         for line in msg.get_payload().split('\n'):
             self._parseLine(line)
