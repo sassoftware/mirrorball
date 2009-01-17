ed623ef9646c17cf46195be68d74022464a7e454
Elliot Peele <https://issues.rpath.com/>
2009-01-16 23:26:51 -0500

    add satis and jira client
    committer: Elliot Peele <https://issues.rpath.com/>

---
+++ updatebot/lib/jira.py
@@ -0,0 +1,36 @@
+#
+# Copyright (c) 2009 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+
+import pyjira
+
+class JiraClient(pyjira.JiraClient):
+    """
+    Basic SOAP based Jira client.
+    """
+
+    def __init__(self, cfg):
+        self._cfg = cfg
+        self._wsdl = '%s/rpc/soap/jirasoapservice-v2?wsdl' % cfg.jiraUrl
+        self._login = (cfg.jiraUser, cfg.jiraPassword)
+
+        pyjira.JiraClient.__init__(self, self._wsdl, self._login)
+
+    def addComment(self, issueKey, body):
+        """
+        Add a comment to an issue, using the security level specified in the
+        config object.
+        """
+
+        pyjira.JiraClient.addComment(self, issueKey, body,
+            level=cfg.jiraSecurityGroup)
---
+++ updatebot/lib/satisclient.py
@@ -0,0 +1,107 @@
+#
+# Copyright (c) 2009 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+
+"""
+Module for interacting with satis.
+"""
+
+from satis import types
+from satis import util as satisutil
+from satis.client.http import HTTPClient
+from satis.types import Or, And, Equals, NewerThan
+
+class SatisClient(object):
+    """
+    Base class for interacting with satis.
+    """
+
+    def __init__(self, url):
+        self._url = url
+        self._client = HTTPClient(self._url)
+
+
+class AutoBuild(SatisClient):
+    """
+    Class for managing autobuild related metadata in satis.
+    """
+
+    def __init__(self, url, productName):
+        SatisClient.__init__(self, url)
+        self._productName = productName
+        self._type = 'com.rpath.autobuild.checkdata'
+
+    def setUUID(self, label, uuid):
+        """
+        Set autobuild checkdata.
+        """
+
+        symb = types.Symbol()
+        symb['type'] = self._type
+        symb['autobuild-symbol-version'] = 0
+        symb['autobuild-product-name'] = self._productName
+        symb['autobuild-label'] = label.asString()
+        symb['autobuild-check-uuid'] = uuid
+        self._client.add(symb)
+
+    def getUUID(self, label):
+        """
+        Get the last checkdata for a given label.
+        """
+
+        matcher = And(Equals('type', self._type),
+                      Equals('autobuild-label', label.asString()),
+                      Equals('autobuild-product-name', self._productName))
+        fltr = types.Filter(matcher, order=types.ORDER_DESC, limit=1)
+        sub = types.Subscription(fltr)
+        req = self._client.subscribe(sub)
+
+        if req:
+            return req[0].symbol['autobuild-check-uuid']
+        return satisutil.makeUUID()
+
+
+class ConaryCommits(SatisClient):
+    """
+    Interface for searching satis for commits to a given label.
+    """
+
+    def __init__(self, url, productName):
+        SatisClient.__init__(self, url)
+        self._rules = ( Equals('type', 'com.rpath.commit.conary.trove'),
+                        Equals('trove-type', 'source') )
+        self._autobuild = AutoBuild(self._url, productName)
+
+    def search(self, label, getMarked=False):
+        """
+        Query satis for commits to the given label.
+        @param label: conary label
+        @type label: conary.versions.Label
+        @param getMarked: to return all commits or just the commits since the
+                          last check.
+        @type getMarked: boolean
+        """
+
+        uuid = self._autobuild.getUUID(label)
+
+        rules = list(self._rules)
+        rules.append(Equals('trove-label', label.asString()))
+        matcher = And(*rules)
+
+        fltr = types.Filter(matcher, order=types.ORDER_ASC, token=uuid,
+                            getMarked=getMarked)
+        sub = types.Subscription(fltr, token=uuid, markOnSend=True)
+        req = self._client.subscribe(sub)
+
+        self._autobuild.setUUID(label, uuid)
+        return req
