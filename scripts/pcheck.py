1925362da4e6a7d7aa1b19c70306da04b2b2a30c
Elliot Peele <https://issues.rpath.com/>
2009-01-18 23:07:54 -0500

    promote script to check rpl:1-rpl:1-qa promote
    committer: Elliot Peele <https://issues.rpath.com/>

---
+++ scripts/pcheck.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import sys
+from conary.lib import util
+sys.excepthook = util.genExcepthook()
+
+from conary import versions
+from conary import callbacks
+from conary import conarycfg
+from conary import conaryclient
+
+cfg = conarycfg.ConaryConfiguration()
+cfg.setContext('1-binary')
+
+client = conaryclient.ConaryClient(cfg)
+
+frmlabel = versions.VersionFromString('/conary.rpath.com@rpl:devel//1')
+groupTrvs = client.repos.findTrove(['conary.rpath.com@rpl:1', ], ('group-os', frmlabel, None))
+
+groupTrvs.sort()
+groupTrvs.reverse()
+
+trvs = [ x for x in groupTrvs if x[1] == groupTrvs[0][1] ]
+
+labelMap = {
+    versions.VersionFromString('/conary.rpath.com@rpl:devel//1'):
+    versions.VersionFromString('/conary.rpath.com@rpl:devel//1-qa'),
+    versions.VersionFromString('/conary.rpath.com@rpl:devel//1-xen'):
+    versions.VersionFromString('/conary.rpath.com@rpl:devel//1-xen-qa'),
+}
+
+cb = conaryclient.callbacks.CloneCallback(cfg)
+success, cs = client.createSiblingCloneChangeSet(
+        labelMap,
+        trvs,
+        cloneSources=True,
+        callback=cb)
+
+import epdb
+epdb.st()
+
+
+
