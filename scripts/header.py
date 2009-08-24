62388ad0e537cdc8cad53f6c3a53f20251e0f8a8
Elliot Peele <https://issues.rpath.com/>
2009-08-24 15:11:54 -0400

    print path info for conary and mirrorball

--- scripts/header.py
+++ scripts/header.py
@@ -18,6 +18,15 @@ import sys
 mirrorballDir = os.environ['HOME'] + '/hg/mirrorball'
 sys.path.insert(0, mirrorballDir)
 
+if 'CONARY_PATH' in os.environ:
+    sys.path.insert(0, os.environ['CONARY_PATH'])
+
+import conary
+import updatebot
+
+print >>sys.stderr, 'using conary from', os.path.dirname(conary.__file__)
+print >>sys.stderr, 'using updatebot from', os.path.dirname(updatebot.__file__)
+
 from conary.lib import util
 sys.excepthook = util.genExcepthook()
 
