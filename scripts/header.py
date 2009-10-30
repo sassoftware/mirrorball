2dd5730a1b4ac91dbbe49af518cb7d46ab8d8fb1
Elliot Peele <https://issues.rpath.com/>
2009-10-30 11:16:53 -0400

    adapt to new interface

--- scripts/header.py
+++ scripts/header.py
@@ -46,7 +46,7 @@ log.addRootLogger()
 cfg = config.UpdateBotConfig()
 cfg.read(mirrorballDir + '/config/%s/updatebotrc' % sys.argv[1])
 
-builder = build.Builder(cfg, saveChangeSets=tempfile.mkdtemp(), sanityCheckCommits=True)
+builder = build.Builder(cfg, saveChangeSets=tempfile.mkdtemp())
 
 def displayTrove(nvf):
     flavor = ''
