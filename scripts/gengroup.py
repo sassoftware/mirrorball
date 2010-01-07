09aea18a44f1a63c84eec83dc352331cb5624a14
Elliot Peele <https://issues.rpath.com/>
2010-01-07 13:37:55 -0500

    add copyright info and validate group model

--- scripts/gengroup.py
+++ scripts/gengroup.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+#
+# Copyright (c) 2009-2010 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+#
+
+"""
+Script for generating group model from current state of the repository.
+"""
 
 import os
 import sys
@@ -49,26 +65,27 @@ for k1, k2 in itertools.izip(sorted(troves), sorted(allTroves)):
         slog.error('unhandled flavor found %s' % k1)
         raise RuntimeError
 
-import epdb; epdb.st()
+#import epdb; epdb.st()
 
 for name, vf in troves.iteritems():
     if ':' in name or bot._updater._fltrPkg(name):
         continue
 
     versions = vf.keys()
+    assert len(versions) == 1
     versions.sort()
     version = versions[-1]
     flavors = vf[version]
 
-    if name == 'kernel':
-        import epdb; epdb.st()
+    mgr.addPackage(name, version, flavors)
 
-        mgr.addPackage(name, version, flavors)
-
-import epdb; epdb.st()
+#import epdb; epdb.st()
 
 mgr.setVersion('0')
 mgr.setErrataState('0')
+mgr._copyVersions()
+mgr._validateGroups()
+mgr._helper.setModel(mgr._sourceName, mgr._groups)
 #mgr._commit()
 #mgr.build()
 
