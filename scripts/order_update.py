7c1874ef5211b0a8c261402c8cb7a257d7c6e51b
Elliot Peele <https://issues.rpath.com/>
2010-05-20 14:04:01 -0400

    allow for multiple advisory sources

--- scripts/order_update.py
+++ scripts/order_update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2009 rPath, Inc.
+# Copyright (c) 2009-2010 rPath, Inc.
 #
 # This program is distributed under the terms of the Common Public License,
 # version 1.0. A copy of this license should have been distributed with this
@@ -32,10 +32,9 @@ print >>sys.stderr, 'using updatebot from', os.path.dirname(updatebot.__file__)
 from conary.lib import util
 sys.excepthook = util.genExcepthook()
 
-import rhnmirror
-
 from updatebot import config
 from updatebot import ordered
+from updatebot import pkgsource
 from updatebot import log as logSetup
 
 logSetup.addRootLogger()
@@ -58,13 +57,45 @@ confDir = mirrorballDir + '/config/' + platform
 cfg = config.UpdateBotConfig()
 cfg.read(confDir + '/updatebotrc')
 
-mcfg = rhnmirror.MirrorConfig()
-mcfg.read(confDir + '/erratarc')
+fltr = None
 
-errata = rhnmirror.Errata(mcfg)
+if cfg.platformName == 'rhel':
+    import rhnmirror
+
+    mcfg = rhnmirror.MirrorConfig()
+    mcfg.read(confDir + '/erratarc')
+
+    errata = rhnmirror.Errata(mcfg)
+elif cfg.platformName == 'sles':
+    from errata.sles import AdvisoryManager as Errata
+
+    pkgSource = pkgsource.PackageSource(cfg)
+    errata = Errata(pkgSource)
+
+    def fltr(sourceSet):
+        removed = set()
+        removedNames = set()
+        pkgSource.load()
+        for src, bins in pkgSource.srcPkgMap.iteritems():
+            # filter out packages that we don't handle right now.
+            if ([ x for x in bins if 'kmp' in x.name ] or
+                # the kernel needs a recipe
+                'kernel' in src.name):
+
+                removedNames.add(src.name)
+
+        for src, bins in pkgSource.srcPkgMap.iteritems():
+            if src.name in removedNames:
+                removed.add(src)
+
+        return sourceSet - removed
+
+else:
+    raise RuntimeError, 'no errata source found for %s' % cfg.platformName
+
 errata.fetch()
 
 bot = ordered.Bot(cfg, errata)
-pkgMap = bot.update(restoreFile=restoreFile)
+pkgMap, failures = bot.update(fltr=fltr)
 
 import epdb; epdb.st()
