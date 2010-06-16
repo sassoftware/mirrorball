0b7c7095c73040b98cda1bf2c3a7d892da3179d4
Elliot Peele <https://issues.rpath.com/>
2010-06-16 10:18:45 -0400

    cleanup script

--- scripts/creategroup.py
+++ scripts/creategroup.py
@@ -16,28 +16,32 @@
 import os
 import sys
 
-mirrorballDir = os.path.abspath('../')
-sys.path.insert(0, mirrorballDir)
+if __name__ == '__main__':
+    mirrorballDir = os.path.abspath('../')
+    sys.path.insert(0, mirrorballDir)
 
-if 'CONARY_PATH' in os.environ:
-    sys.path.insert(0, os.environ['CONARY_PATH'])
+    if 'CONARY_PATH' in os.environ:
+        sys.path.insert(0, os.environ['CONARY_PATH'])
 
-import rmake
-import conary
-import updatebot
+    import rmake
+    import conary
+    import updatebot
 
-print >>sys.stderr, 'using conary from', os.path.dirname(conary.__file__)
-print >>sys.stderr, 'using rmake from', os.path.dirname(rmake.__file__)
-print >>sys.stderr, 'using updatebot from', os.path.dirname(updatebot.__file__)
+    print >>sys.stderr, 'using conary from', os.path.dirname(conary.__file__)
+    print >>sys.stderr, 'using rmake from', os.path.dirname(rmake.__file__)
+    print >>sys.stderr, 'using updatebot from', os.path.dirname(updatebot.__file__)
 
-from conary.lib import util
-sys.excepthook = util.genExcepthook()
+    from conary.lib import util
+    sys.excepthook = util.genExcepthook()
+
+    from updatebot import log as logSetup
+    logSetup.addRootLogger()
 
 import logging
 
 from updatebot import OrderedBot
 
-log = logging.getLogger('tmplogger')
+log = logging.getLogger('create group')
 
 class Bot(OrderedBot):
     def generateInitialGroup(self):
@@ -60,28 +64,41 @@ class Bot(OrderedBot):
                 for flv in flavors:
                     trvs.setdefault(name, dict()).setdefault(version, set()).add(flv)
 
+        # Filter out sources, groups, and components; gathering all of the
+        # package versions.
         pkgs = set()
         for name, vMap in trvs.iteritems():
             if name.endswith(':source'):
+                continue
+            if name.startswith('group-'):
                 continue
             name = name.split(':')[0]
             for version, flavors in vMap.iteritems():
                 data = (name, version, tuple(flavors))
                 pkgs.add(data)
 
+        # Get the latest group model.
         group = self._groupmgr.getGroup()
 
+        # Remove the existing packages group.
+        group._groups.pop('group-packages', None)
+
+        # Add content to the packages group, which will cause a new model to
+        # be created.
         for name, version, flavors in pkgs:
             log.info('adding %s=%s' % (name, version))
             for flv in flavors:
                 log.info('\t%s' % flv)
             group.addPackage(name, version, flavors)
 
+        # Set the errata state and version to some defaults.
         group.errataState = 0
-        group.version = 'sp3'
+        group.version = '0'
 
+        # Remove the existing standard group if there is one.
         group._groups.pop('group-standard', None)
 
+        # Run through all of the adds and removes for the standard group.
         removals = set()
         nevras = dict([ (x.getNevra(), y)
             for x, y in self._pkgSource.srcPkgMap.iteritems() ])
@@ -94,16 +111,26 @@ class Bot(OrderedBot):
 
             removals |= set(self._cfg.updateRemovesPackages.get(updateId, ()))
 
+        # Remove any packages that would have normally been removed at this
+        # errataState.
         for name in removals:
             group.removePackage(name, missingOk=True)
 
+        # Sanity check the group model and write out the current state so that
+        # you can do a local test cook.
         group._copyVersions()
         group._sanityCheck()
         group._mgr._persistGroup(group)
 
+        # You probably want to do a test cook if your groups here. It would be
+        # nice if mirrorball could just do this for you, but it can't right now.
+        # To run a test cook take a look at group._mgr._helper._checkoutCache to
+        # find the directory where the checkout is and then run cvc cook from
+        # that directory.
         import epdb; epdb.st()
 
-        group.commit()
+        # Commit and build the group.
+        group = group.commit()
         built = group.build()
 
         import epdb; epdb.st()
@@ -112,16 +139,11 @@ class Bot(OrderedBot):
 
 if __name__ == '__main__':
     from updatebot import config
-    from updatebot import log as logSetup
-
-    logSetup.addRootLogger()
-
-    log = logging.getLogger('create group')
 
     cfg = config.UpdateBotConfig()
     cfg.read(mirrorballDir + '/config/%s/updatebotrc' % sys.argv[1])
 
     bot = Bot(cfg, None)
-    changes = bot.generateInitialGroup()
+    trvMap = bot.generateInitialGroup()
 
     import epdb; epdb.st()
