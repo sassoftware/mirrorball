ab43dfbb286d7f0384c8125eb084071d511a2eab
Andy Grimm <https://issues.rpath.com/>
2010-10-08 14:00:59 -0400

    add derivedcapsule to superclass list; switch back to buildstamps for centos

--- errata/centos.py
+++ errata/centos.py
@@ -113,7 +113,8 @@ class AdvisoryManager(common.AdvisoryManager):
         # Sort packages by build timestamp.
         slices = {}
         for srcPkg in self._pkgSource.srcPkgMap:
-            updateId = slice(int(srcPkg.fileTimestamp or srcPkg.buildTimestamp))
+            # updateId = slice(int(srcPkg.fileTimestamp or srcPkg.buildTimestamp))
+            updateId = slice(int(srcPkg.buildTimestamp))
             # If package comes from a base path, override updateId
             for basePath in self._pkgSource._cfg.repositoryBasePaths:
                 if basePath[1].match(srcPkg.location) is not None:
