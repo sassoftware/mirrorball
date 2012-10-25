fa9286ee22b28c5914ad869e0a659daff3c67fd2
Elliot Peele <https://issues.rpath.com/>
2012-10-25 14:24:17 -0400

    relicense as MIT

--- scripts/hardlink.py
+++ scripts/hardlink.py
@@ -1,49 +1,26 @@
 #!/usr/bin/python
+#
+# Copyright (c) rPath, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
 
-# hardlink - Goes through a directory structure and creates hardlinks for
-# files which are identical.
-#
-# Copyright (C) 2003 - 2007  John L. Villalovos, Hillsboro, Oregon
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc., 59
-# Temple Place, Suite 330, Boston, MA  02111-1307, USA.
-#
-#
-# ------------------------------------------------------------------------
-# John Villalovos
-# email: john@sodarock.com
-# http://www.sodarock.com/
-#
-# Inspiration for this program came from the hardlink.c code. I liked what it
-# did but did not like the code itself, to me it was very unmaintainable.  So I
-# rewrote in C++ and then I rewrote it in python.  In reality this code is
-# nothing like the original hardlink.c, since I do things quite differently.
-# Even though this code is written in python the performance of the python
-# version is much faster than the hardlink.c code, in my limited testing.  This
-# is mainly due to use of different algorithms.
-#
-# Original inspirational hardlink.c code was written by:  Jakub Jelinek
-# <jakub@redhat.com>
-#
-# ------------------------------------------------------------------------
-#
-# TODO:
-#   *   Thinking it might make sense to walk the entire tree first and collect
-#       up all the file information before starting to do comparisons.  Thought
-#       here is we could find all the files which are hardlinked to each other
-#       and then do a comparison.  If they are identical then hardlink
-#       everything at once.
 
 import getopt
 import os
